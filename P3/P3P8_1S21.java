5fa5f52c4b1313e5053f391e09d88e8bf68bb612
RafaelRamosO <144179411+RafaelRamosO@users.noreply.github.com>
2023-11-28 15:33:09 -0600

    Add files via upload

---
+++ P3/P3P8_1S21.java
@@ -0,0 +1,17 @@
+public class P3P8_1S21 {
+	public static void main(String[] args) {
+		int c = a + b;
+		if (c<200){
+			System.out.println(c);
+		}else {
+
+			f(b,c);
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("0\n1");
+		f(0,1);
+	}
+}
+	
+	
